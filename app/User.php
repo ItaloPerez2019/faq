e071a37e96c3e790921899f1a46ff69ff10d00f1
italoperez2019 <Itl00147441!>
2018-11-20 12:10:01 -0500

    Added user profile migrate,model and updated user seeder to create user profile.

--- app/User.php
+++ app/User.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
+use  Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
@@ -52,4 +52,10 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+     public function profile(){
+         return $this->hasOne('App\Profile');
+
+     }
+
 }
